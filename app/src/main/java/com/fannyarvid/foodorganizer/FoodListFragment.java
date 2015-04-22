6ba033d1058dd011c5f47e1c3420afd48e1a0159
ArvidBaa <ArvidBaa@users.noreply.github.com>
2015-04-22 16:28:09 +0200

    Added FoodListFragmet class, IngredientListFragment class and started working on the implementations of the sliding

---
+++ app/src/main/java/com/fannyarvid/foodorganizer/FoodListFragment.java
@@ -0,0 +1,30 @@
+package com.fannyarvid.foodorganizer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by FannyArvid on 2015-04-22.
+ */
+public class FoodListFragment extends Fragment {
+    /**
+     * The argument key for the page number this fragment represents.
+     */
+    public static final String ARG_PAGE = "page";
+
+    @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        View rootView = inflater.inflate(
+                R.layout.fragment_food_list, container, false);
+        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                "Test food");
+        return rootView;
+    }
+}
