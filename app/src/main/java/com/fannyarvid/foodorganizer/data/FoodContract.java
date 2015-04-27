8f1a8f1b7f082502425c85e1ee08fffd4ec95236
ArvidBaa <arvid.baarnhielm@gmail.com>
2015-04-27 17:12:10 +0200

    Continued work on the FoodDatabase. Added provider authorities in AndroidManifest.xml. Added normalizeDate() method to FoodContract. Wrote getType(), query(), insert(), update() and delete() methods in FoodProvider. Added test classes TestProvider and TestUtilities for testing purposes, much left to add to those.

--- app/src/main/java/com/fannyarvid/foodorganizer/data/FoodContract.java
+++ app/src/main/java/com/fannyarvid/foodorganizer/data/FoodContract.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.text.format.Time;
 
 /**
  * Created by FannyArvid on 2015-04-24.
@@ -17,6 +18,36 @@ public class FoodContract {
     public static final String PATH_BOX = "box";
     public static final String PATH_INGREDIENT = "ingredient";
     public static final String PATH_FOOD = "link";
+
+    public static long normalizeDate(long startDate) {
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+    public static final class FoodEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FOOD).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FOOD;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FOOD;
+
+        public static final String TABLE_NAME = "food";
+
+        public static final String COLUMN_BOX_KEY = "box_id";
+        public static final String COLUMN_INGREDIENT_KEY = "ingredient_id";
+
+        public static Uri buildFoodUri (long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static long getBoxNoFromUri(Uri uri) {
+            return Long.parseLong(uri.getPathSegments().get(0));
+        }
+    }
 
     public static final class BoxEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
@@ -59,25 +90,6 @@ public class FoodContract {
         public static final String COLUMN_STORAGE_TIME_FREEZER = "storage_time_freezer";
 
         public static Uri buildIngredientUri (long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-        }
-    }
-
-    public static final class FoodEntry implements BaseColumns {
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FOOD).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FOOD;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FOOD;
-
-        public static final String TABLE_NAME = "food";
-
-        public static final String COLUMN_BOX_KEY = "box_id";
-        public static final String COLUMN_INGREDIENT_KEY = "ingredient_id";
-
-        public static Uri buildFoodUri (long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
