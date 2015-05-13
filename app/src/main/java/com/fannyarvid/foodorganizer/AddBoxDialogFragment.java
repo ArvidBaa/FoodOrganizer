2f3197b1682b88359c50a794d4d806597578e3f0
ArvidBaa <arvid.baarnhielm@gmail.com>
2015-05-13 16:20:00 +0200

    Added checkBoxe to the list view in addBoxDialogFragment and added methods so that all checked items id gets registered when pressing "add" button. Also made some other minor changes.

--- app/src/main/java/com/fannyarvid/foodorganizer/AddBoxDialogFragment.java
+++ app/src/main/java/com/fannyarvid/foodorganizer/AddBoxDialogFragment.java
@@ -4,18 +4,26 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.Time;
-import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.fannyarvid.foodorganizer.data.FoodContract;
+
+import java.util.ArrayList;
 
 /**
  * Created by FannyArvid on 2015-05-11.
@@ -26,11 +34,11 @@ public class AddBoxDialogFragment extends DialogFragment {
 
     public interface AddBoxDialogListener {
         public void onAddBoxDialogPositiveClick(
-                String boxName, int date, int storageType, int hasBeenInFreezer, long[] ingredientsId);
+                String boxName, int date, int storageType, int hasBeenInFreezer, ArrayList<Integer> ingredientsId);
     }
 
     AddBoxDialogListener mListener;
-    private IngredientFragment.IngredientAdapter mIngredientAdapter;
+    private IngredientCheckboxAdapter mIngredientAdapter;
     private EditText mBoxName;
     private RadioGroup mStorageButtonGroup;
     private ListView mIngredientsList;
@@ -64,7 +72,7 @@ public class AddBoxDialogFragment extends DialogFragment {
                 null
         );
 
-        mIngredientAdapter = new IngredientFragment.IngredientAdapter(getActivity(), cur, 0);
+        mIngredientAdapter = new IngredientCheckboxAdapter(getActivity(), cur, 0);
         mIngredientsList.setAdapter(mIngredientAdapter);
 
         builder.setView(dialogView)
@@ -81,19 +89,15 @@ public class AddBoxDialogFragment extends DialogFragment {
                         int storageType = mStorageButtonGroup.getCheckedRadioButtonId(); // 0 = fridge, 1 = freezer
                         int hasBeenInFreezer = storageType; // 0 = false, 1 = true. true IF storageType has been 1 at any time.
 
-                        // TODO: change from dummy data to real data here
-
-                       long[] ingredientsId = mIngredientsList.getCheckedItemIds();
-                        for(int i = 0; i < ingredientsId.length; i++) {
-                            Log.d(LOG_TAG, "ingredientsId " + i + ": " + ingredientsId[i]);
-                        }
+                        ArrayList<Integer> ingredientsId = mIngredientAdapter.getCheckedId();
 
                         mListener.onAddBoxDialogPositiveClick(
                                 boxName,
                                 julianDay,
                                 storageType,
                                 hasBeenInFreezer,
-                                ingredientsId);
+                                ingredientsId
+                        );
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -103,5 +107,70 @@ public class AddBoxDialogFragment extends DialogFragment {
                     }
                 });
         return builder.create();
+    }
+
+    public static class IngredientCheckboxAdapter extends CursorAdapter implements CompoundButton.OnCheckedChangeListener {
+
+        private static final String LOG_TAG = IngredientCheckboxAdapter.class.getSimpleName();
+
+        SparseBooleanArray mCheckStates = new SparseBooleanArray();
+
+        public IngredientCheckboxAdapter(Context context, Cursor cursor, int flags) {
+            super(context, cursor, flags);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(R.layout.list_item_ingredient_checkbox, parent, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+
+            // TODO: Use the viewHolder class?
+            //IngredientViewHolder viewHolder = (IngredientViewHolder) view.getTag();
+            //viewHolder.nameView.setText("Test ingredient");
+
+            CheckBox checkBox = (CheckBox) view.findViewById(R.id.ingredient_list_item_name_checkbox);
+            String ingredientNameStr = cursor.getString(IngredientFragment.COL_INGREDIENT_NAME);
+            checkBox.setOnCheckedChangeListener(this);
+            checkBox.setTag(cursor.getPosition());
+            checkBox.setText(ingredientNameStr);
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            mCheckStates.put((Integer) buttonView.getTag(),isChecked);
+        }
+
+        public boolean isChecked(int position) {
+            return mCheckStates.get(position, false);
+        }
+
+        public void setChecked(int position, boolean isChecked) {
+            mCheckStates.put(position, isChecked);
+        }
+
+        public void toggle(int position) {
+            setChecked(position, !isChecked(position));
+        }
+
+        public ArrayList<Integer> getCheckedId() {
+            ArrayList<Integer> returnId = new ArrayList<Integer>(mCheckStates.size());
+            for (int i = 0; i < mCheckStates.size(); i++) {
+                if(mCheckStates.valueAt(i)) {
+                    returnId.add(mCheckStates.keyAt(i));
+                }
+            }
+            return returnId;
+        }
+
+        public static class IngredientViewHolder {
+            public final TextView nameView;
+
+            public IngredientViewHolder(View view) {
+                nameView = (TextView) view.findViewById(R.id.ingredient_list_item_name);
+            }
+        }
     }
 }
